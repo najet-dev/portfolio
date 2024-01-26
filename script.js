98804946d26f64ae9230f37a56fe7547a54a1ffc
Najet Ramoul <najet.ramoul68@gmail.com>
2024-01-27 00:16:06 +0100

    More details added to completed projects. The work isn't finished yet: the pages still need to be reorganized to include the methodology, duration and development tools.

--- script.js
+++ script.js
@@ -194,19 +194,22 @@ const texts = {
     descriptionSteamer:"<p>Application de rencontres synchronisée avec STEAM, mettant en relation des utilisateurs en fonction de leurs jeux communs au cours des 15 derniers jours.</p>",
     descriptionAvenir:"<p>L'application mobile permet aux utilisateurs de rédiger des messages positifs et de planifier des rappels quotidiens basés sur ces messages.</p>",
     projectSteamer: "<span class='pink-text'> Steamer</span>",
-    business: "<strong>Entreprise</strong> : Mithra production",
+    business: "<strong>Entreprise :</strong> Mithra production",
+    duration: "<strong>Durée du projet :</strong> 6 semaines",
+    methodology: "<strong>Méthodologie :</strong> Adoptant une approche de projet agile, nous avons suivi le framework Scrum au sein d'une équipe de 4 personnes. Les activités étaient organisées en sprints d'une semaine, favorisant ainsi une collaboration efficace et des itérations rapides.",
+    management: "<strong>Outils de developpement :</strong>Nous avons utilisé des outils tels que Confluence pour la documentation, Jira pour le suivi des tâches, et Git pour la gestion de version.",
     techno:
-      " <strong>Les technologies</strong> : Ionic pour le frontend, NestJS pour le backend et MariaDB comme base de données.",
+      " <strong>Les technologies :</strong> Ionic pour le frontend, NestJS pour le backend et MariaDB comme base de données.",
     projectOne:
-      "<p>Dans ce projet, j'ai conçu la page de profil et créé la page 'favorites' avec des badges pour les nouveaux likes et matchs. J'ai aussi géré l'enregistrement des informations sur les likes, dislikes, et les matchs entre utilisateurs, incluant une suppression automatique des dislikes après 30 jours.</p>",
+      "<p><strong>Réalisations Spécifiques : </strong>Dans ce projet, j'ai conçu la page de profil et créé la page 'favorites' avec des badges pour les nouveaux likes et matchs. J'ai aussi géré l'enregistrement des informations sur les likes, dislikes, et les matchs entre utilisateurs, incluant une suppression automatique des dislikes après 30 jours.</p>",
     projectAvenir: "<span class='pink-text'> Avenir</span>",
-    avenir: "<strong>Entreprise</strong> : Avenir",
+    avenir: "<strong>Entreprise :</strong> Avenir",
     technos:
-      " <strong>Les technologies</strong> : Ionic pour le frontend, NestJS pour le backend et MariaDB comme base de données.",
+      " <strong>Les technologies :</strong> Ionic pour le frontend, NestJS pour le backend et MariaDB comme base de données.",
     projectTwo:"<p>Mon rôle dans ce projet consistait à concevoir un formulaire permettant aux utilisateurs d'ajouter des messages positifs, tout en mettant en place une fonctionnalité de rappels quotidiens personnalisables. Cette option permettait aux utilisateurs de programmer des rappels en fonction de leurs propres messages inspirants.</p>",
     projectToolbox: "<span class='pink-text'> La boîte à Outils</span>",
-    toolbox: "<strong>Entreprise</strong> : Université de Haute Alsace",
-    technoToolbox:"<strong>Les technologies</strong> : JavaScript pour le frontend, PHP et WordPress pour le backend et MySQL comme base de données.",
+    toolbox: "<strong>Entreprise :</strong>Université de Haute Alsace",
+    technoToolbox:"<strong>Les technologies :</strong> JavaScript pour le frontend, PHP et WordPress pour le backend et MySQL comme base de données.",
     projectBox: "<p>En tant que développeuse unique du projet 'La Boîte à Outils', j'ai effectué la migration des fonctionnalités vers un plugin. Cela a impliqué le transfert et la refonte complète d'aspects essentiels tels que l'affichage des outils pédagogiques, la gestion du panier et la sauvegarde des sélections. Mon objectif principal était d'optimiser le code pour une meilleure performance et d'améliorer l'expérience utilisateur du site. Pour découvrir le résultat final, veuillez cliquer sur le bouton ci-dessous.</p>",
     contactMe: "Contactez-<span class='pink-text'>Moi</span> !",
     placeholders: {
@@ -235,21 +238,25 @@ const texts = {
     descriptionSteamer: "<p>Dating application synchronized with STEAM, connecting users based on their shared games over the past 15 days.</p>",
     descriptionAvenir:"<p>The mobile app allows users to write positive messages and schedule daily reminders based on these messages.</p>",
     projectSteamer: "<span class='pink-text'>Steamer</span>",
-    business: "<strong>Company</strong> : Mithra production",
+    business: "<strong>Company :</strong>Mithra production",
+    duration: "<strong>Project duration : </strong> 6 weeks.",
+
+    methodology: "<strong>Méthodologie : </strong>Adopting an agile project approach, we followed the Scrum framework as part of a four person team. Activities were organized in one-week sprints, promoting effective collaboration and rapid iterations.",
+    management: "<strong>Development tools :</strong>We used tools such as Confluence for documentation, Jira for task tracking, and Git for version management.",
     techno:
-      "<strong>The technologies</strong> : Ionic for the frontend, NestJS for the backend and  MariaDB for the database.",
+      "<strong>The technologies :</strong>Ionic for the frontend, NestJS for the backend and  MariaDB for the database.",
     projectOne:
       "<p>In this project, I designed the profile page and created the 'favorites' page with badges for new likes and matches. I also managed the recording of information on likes, dislikes, and matches between users, including automatic deletion of dislikes after 30 days.</p>",
     projectAvenir: "<span class='pink-text'> Avenir</span>",
-    avenir: "<strong>Company</strong> : Avenir",
+    avenir: "<strong>Company :</strong>Avenir",
     technos:
-      "<strong>The technologies</strong> : Ionic for the frontend, NestJS for the backend and  MariaDB for the database.",
+      "<strong>The technologies :</strong>Ionic for the frontend, NestJS for the backend and  MariaDB for the database.",
     projectTwo:
       "<p>My role in this project was to design a form allowing users to add positive messages, while implementing a customizable daily reminders functionality. This option allowed users to schedule reminders based on their own inspirational messages.</p>",
     projectToolbox: "<span class='pink-text'> La boîte à Outils</span>",
-    toolbox: "<strong>Company</strong> : Université de Haute Alsace",
+    toolbox: "<strong>Company :</strong>Université de Haute Alsace",
     technoToolbox:
-    "<strong>The technologies</strong> : JavaScript for the frontend, PHP and WordPress for the backend, and MySql as the database.",
+    "<strong>The technologies :</strong>JavaScript for the frontend, PHP and WordPress for the backend, and MySql as the database.",
     projectBox:
     "<p>As part of the 'Toolbox' project, I identified and improved essential functionalities. Through the development of a custom plugin, I facilitated a more efficient integration while ensuring a smooth transition of existing functionality. My goal was to optimize the user experience, and as the sole developer on the project, my work significantly improved the performance and usability of the Toolbox.</p>",
     contactMe: "Contact <span class='pink-text'>Me</span> !",
@@ -308,6 +315,9 @@ function updateLanguage() {
     technoToolbox: document.getElementById("technoToolbox"),
     projectBox: document.getElementById("projectBox"),
     business: document.getElementById("business"),
+    duration: document.getElementById("duration"),
+    methodology: document.getElementById("methodology"),
+    management: document.getElementById("management"),
     techno: document.getElementById("techno"),
     projectOne: document.getElementById("projectOne"),
     projectAvenir: document.getElementById("projectAvenir"),
